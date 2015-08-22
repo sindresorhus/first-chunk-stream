4f09e5f2ca03d8af0adc99e2bf938b0a155682f4
Nicolas Froidure <nicolas.froidure@insertafter.com>
2015-08-22 09:38:39 +0200

    Changing tests for the new API

--- test.js
+++ test.js
@@ -10,50 +10,104 @@ describe('firstChunk()', function () {
 
 		describe('for ' + version + ' streams', function () {
 
-			it('should should work for a single oversized chunk', function (cb) {
-				streamtest[version].fromChunks([content])
-					.pipe(firstChunkStream(function (chunk, enc, cb) {
-						this.push(chunk);
-						cb();
-					}))
-					.pipe(streamtest[version].toText(function (err, text) {
-						if(err) {
-							return done(err);
-						}
-						assert.deepEqual(text, content);
-						cb();
-					}));
+			describe('and leaving content as is', function () {
+
+				it('should work for a single oversized chunk', function (cb) {
+					streamtest[version].fromChunks([content])
+						.pipe(firstChunkStream({firstChunkSize: 7}, function (chunk, enc, cb) {
+							assert.equal(chunk.toString('utf-8'), content.substr(0, 7));
+							cb(null, chunk);
+						}))
+						.pipe(streamtest[version].toText(function (err, text) {
+							if(err) {
+								return done(err);
+							}
+							assert.deepEqual(text, content);
+							cb();
+						}));
+				});
+
+
+				it('should work for required size chunk', function (cb) {
+					streamtest[version].fromChunks([content.substr(0, 7), content.substr(7)])
+						.pipe(firstChunkStream({firstChunkSize: 7}, function (chunk, enc, cb) {
+							assert.equal(chunk.toString('utf-8'), content.substr(0, 7));
+							cb(null, chunk);
+						}))
+						.pipe(streamtest[version].toText(function (err, text) {
+							if(err) {
+								return done(err);
+							}
+							assert.deepEqual(text, content);
+							cb();
+						}));
+				});
+
+				it('should work for several small chunks', function (cb) {
+					streamtest[version].fromChunks(content.split(''))
+						.pipe(firstChunkStream({firstChunkSize: 7}, function (chunk, enc, cb) {
+							assert.equal(chunk.toString('utf-8'), content.substr(0, 7));
+							cb(null, chunk);
+						}))
+						.pipe(streamtest[version].toText(function (err, text) {
+							if(err) {
+								return done(err);
+							}
+							assert.deepEqual(text, content);
+							cb();
+						}));
+				});
+
 			});
 
+			describe('and changing content', function () {
 
-			it('should should work for required size chunk', function (cb) {
-				streamtest[version].fromChunks([content.substr(0, 7), content.substr(7)])
-					.pipe(firstChunkStream({minSize: 7}, function (chunk, enc, cb) {
-						this.push(chunk);
-						cb();
-					}))
-					.pipe(streamtest[version].toText(function (err, text) {
-						if(err) {
-							return done(err);
-						}
-						assert.deepEqual(text, content);
-						cb();
-					}));
-			});
+				it('should work when removing the first chunk', function (cb) {
+					streamtest[version].fromChunks([content])
+						.pipe(firstChunkStream({firstChunkSize: 7}, function (chunk, enc, cb) {
+							assert.equal(chunk.toString('utf-8'), content.substr(0, 7));
+							cb(null, new Buffer(''));
+						}))
+						.pipe(streamtest[version].toText(function (err, text) {
+							if(err) {
+								return done(err);
+							}
+							assert.deepEqual(text, content.substr(8));
+							cb();
+						}));
+				});
 
-			it('should should work for several small chunks', function (cb) {
-				streamtest[version].fromChunks(content.split(''))
-					.pipe(firstChunkStream(function (chunk, enc, cb) {
-						this.push(chunk);
-						cb();
-					}))
-					.pipe(streamtest[version].toText(function (err, text) {
-						if(err) {
-							return done(err);
-						}
-						assert.deepEqual(text, content);
-						cb();
-					}));
+
+				it('should work when replacing per a larger chunk', function (cb) {
+					streamtest[version].fromChunks([content.substr(0, 7), content.substr(7)])
+						.pipe(firstChunkStream({firstChunkSize: 7}, function (chunk, enc, cb) {
+							assert.equal(chunk.toString('utf-8'), content.substr(0, 7));
+							cb(null, Buffer.concat([chunk, new Buffer('plop')], 2));
+						}))
+						.pipe(streamtest[version].toText(function (err, text) {
+							if(err) {
+								return done(err);
+							}
+							assert.deepEqual(text, content.substr(0, 7) + 'plop' + content.substr(8));
+							cb();
+						}));
+				});
+
+				it('should work when replacing per a smaller chunk', function (cb) {
+					streamtest[version].fromChunks(content.split(''))
+						.pipe(firstChunkStream({firstChunkSize: 7}, function (chunk, enc, cb) {
+							assert.equal(chunk.toString('utf-8'), content.substr(0, 7));
+							cb(null, new Buffer('plop'));
+						}))
+						.pipe(streamtest[version].toText(function (err, text) {
+							if(err) {
+								return done(err);
+							}
+							assert.deepEqual(text, 'plop' + content.substr(8));
+							cb();
+						}));
+				});
+
 			});
 
 		});
